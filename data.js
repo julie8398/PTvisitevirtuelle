12e1a54c4153c1775ea64ee2f6ac377831904e02
paulinelnh <72135746+paulinelnh@users.noreply.github.com>
2020-12-03 12:30:51 +0100

    modif hotspot nv amphi

--- data.js
+++ data.js
@@ -26,9 +26,9 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.6870784343848602,
-          "pitch": -0.14037612784669307,
-          "rotation": 0.7853981633974483,
+          "yaw": 24.51,
+          "pitch": -0.3,
+          "rotation": 9.5,
           "target": "1-balcon"
         }
       ],
@@ -36,20 +36,14 @@ var APP_DATA = {
         {
           "yaw": -1.1,
           "pitch": -0.15,
-          "title": "Le nouvel amphi",
-          "text": "Le nouvel amphi a été crée en 2018 c'est un endroit fondamentale de la vie Centralienne&nbsp;<br>BLABLABLA"
-        },
-        {
-          "yaw": -2,
-          "pitch": -0.15,
-          "title": "Le nouvel amphi2",
-          "text": "Le nouvel amphi a été crée en 2018 c'est un endroit fondamentale de la vie Centralienne&nbsp;<br>BLABLABLA"
+          "title": "Centrale Marseille",
+          "text": "Centrale Marseille est une jeune école qui est née en 2006. Elle est héritière d’une histoire plus que centenaire. De plus, elle est avant tout une École Centrale, à ce titre elle partage avec les autres Écoles Centrales les mêmes modalités d’accès, le même modèle pédagogique, des relations internationales mutualisées et une conception identique de l’ingénieur. Centrale Marseille est un Établissement Public à Caractère Scientifique, Culturel et Professionnel. Elle est située à Marseille, au cœur du Technopôle de Château-Gombert où se croisent établissements d’enseignement supérieur, centres de recherche et entreprises dans le 13ème arrondissement, au pied du massif de l’Étoile. L’École est facilement accessible en transport en commun (métro et bus) depuis le centre ville de Marseille. Plusieurs résidences étudiantes proposent des logements à proximité.&nbsp;<br>L’École centrale de Marseille est dotée de locaux et d’équipements adaptés aux enjeux de la pédagogie et de la recherche. Centrale Marseille s’engage aussi fortement en faveur de la vie associative des élèves et accueille, chaque année, de nombreuses manifestations.&nbsp;<br>Plus d’information sur le site Internet : https://www.centrale-marseille.fr"
         }
       ]
     },
     {
       "id": "1-balcon",
-      "name": "Balcon", /*titre en haut de la page*/
+      "name": "Passerelle Nouvel Amphi", /*titre en haut de la page*/
       "levels": [
         {
           "tileSize": 256,
