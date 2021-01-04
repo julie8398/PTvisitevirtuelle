3801185f1d322730bb5ef234153bf7fe523ce3ae
paulinelnh <72135746+paulinelnh@users.noreply.github.com>
2021-01-04 20:41:45 +0100

    Ajout passerelle et cours

--- data.js
+++ data.js
@@ -29,7 +29,7 @@ var APP_DATA = {
           "yaw": 24.51,
           "pitch": -0.3,
           "rotation": 9.5,
-          "target": "1-balcon"
+          "target": "3-passerelle-23"
         }
       ],
       "infoHotspots": [
@@ -85,6 +85,94 @@ var APP_DATA = {
     {
       "id": "2-nouvelamphi-20",
       "name": "Nouvel Amphithéâtre",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 5000,
+      "initialViewParameters": {
+        "yaw": -0.22220960032851167,
+        "pitch": -0.13085209605894121,
+        "fov": 1.3, /*1.4 est la limite pour pouvoir dézoomer en position initiale dans ce cas*/
+      },
+      "linkHotspots": [
+        {
+          "yaw": 4.8,
+          "pitch": 0.18,
+          "rotation": 4.7,
+          "target": "1-balcon"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8,
+          "pitch": 0.1,
+          "title": "Présentation de la formation",
+          "text": "L’École centrale de Marseille forme des ingénieurs généralistes à haut niveau scientifique. Centrale Marseille a conçu un cursus unique qui permet aux élèves de devenir les auteurs de leur propre parcours. De nombreuses opportunités sont offertes aux étudiants. En effet les trois premiers semestres sont construits autour d’un rythme unique, à savoir l’alternance de trois semaines académiques et une semaine d’alternance : &nbsp;<br> - Train’ing (ce sont des semaines de professionnalisation) &nbsp;<br> - Alternance entreprise &nbsp;<br> - Alternance recherche &nbsp;<br> - Alternance auto-entreprenariat &nbsp;<br> Les semestres suivants permettent de personnaliser la formation. Centrale Marseille propose une multitude de combinaisons et d’opportunités : semestres thématiques, césure libre ou académique, projets, mobilité internationale, option d’approfondissement, filière métier, stages... C’est l’occasion d’affirmer sa personnalité et de se projeter dans un avenir professionnel. &nbsp;<br> &nbsp;<br> Plus d’informations sur la formation et les nombreuses opportunités ici : https://formation.centrale-marseille.fr/fr"
+        }
+      ]
+    },
+    {
+      "id": "3-passerelle-23",
+      "name": "Passerelle",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 5000,
+      "initialViewParameters": {
+        "yaw": -0.22220960032851167,
+        "pitch": -0.13085209605894121,
+        "fov": 1.3, /*1.4 est la limite pour pouvoir dézoomer en position initiale dans ce cas*/
+      },
+      "linkHotspots": [
+        {
+          "yaw": 14.5,
+          "pitch": -0.2,
+          "rotation": 11,
+          "target": "0-newamphi-60"
+        },
+        {
+          "yaw": 15.1,
+          "pitch": 0.2,
+          "rotation": 0,
+          "target": "2-nouvelamphi-20"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8,
+          "pitch": 0.1,
+          "title": "Présentation de la formation",
+          "text": "L’École centrale de Marseille forme des ingénieurs généralistes à haut niveau scientifique. Centrale Marseille a conçu un cursus unique qui permet aux élèves de devenir les auteurs de leur propre parcours. De nombreuses opportunités sont offertes aux étudiants. En effet les trois premiers semestres sont construits autour d’un rythme unique, à savoir l’alternance de trois semaines académiques et une semaine d’alternance : &nbsp;<br> - Train’ing (ce sont des semaines de professionnalisation) &nbsp;<br> - Alternance entreprise &nbsp;<br> - Alternance recherche &nbsp;<br> - Alternance auto-entreprenariat &nbsp;<br> Les semestres suivants permettent de personnaliser la formation. Centrale Marseille propose une multitude de combinaisons et d’opportunités : semestres thématiques, césure libre ou académique, projets, mobilité internationale, option d’approfondissement, filière métier, stages... C’est l’occasion d’affirmer sa personnalité et de se projeter dans un avenir professionnel. &nbsp;<br> &nbsp;<br> Plus d’informations sur la formation et les nombreuses opportunités ici : https://formation.centrale-marseille.fr/fr"
+        }
+      ]
+    },
+    {
+      "id": "4-salle-de-cours-0",
+      "name": "Salle de cours",
       "levels": [
         {
           "tileSize": 256,
